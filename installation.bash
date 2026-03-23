87fa5f254b9e5bc18979222ae8b97e0a24728c1b
Alvaro Navarro-Pérez <alvaro_navarro@ieee.org>
2026-03-22 23:22:04 -0500

    updated installation structure

--- installation.bash
+++ installation.bash
@@ -40,14 +40,31 @@ sudo chmod +x advanced-mod.bash
 
 cd ..
 rosdep install --from-path src --ignore-src -r -y # Install dependencies 
-colcon build --packages-select sync_sensors_interface -Wno-dev
-colcon build --packages-select cam_lidar_calibration_interface -Wno-dev
-colcon build --packages-select open_dataset_interface -Wno-dev
-colcon build --packages-select pcl_kiss_filter_interface -Wno-dev
+colcon build --packages-select sync_sensors_interface 
+colcon build --packages-select cam_lidar_calibration_interface 
+colcon build --packages-select open_dataset_interface 
+colcon build --packages-select pcl_kiss_filter_interface 
 
 source ~/ros2_ws/install/setup.bash
 
-colcon build --cmake-args -Wno-dev
+colcon build --packages-select cam_lidar_calibration
+colcon build --packages-select create_UV_dataset
+colcon build --packages-select gps_imu
+colcon build --packages-select kiss_icp
+colcon build --packages-select kiss_icp_odometry
+colcon build --packages-select kiss_icp_wrapper
+colcon build --packages-select multi_sensor
+colcon build --packages-select open_dataset
+colcon build --packages-select panoramic_image
+colcon build --packages-select pcl_kiss_filter
+colcon build --packages-select projection_lidar_cameras
+colcon build --packages-select sync_all_sensors_from_rosbag
+colcon build --packages-select sync_sensors
+colcon build --packages-select test_sync_camera_lidar
+colcon build --packages-select trajectory_publisher
+colcon build --packages-select visualize_sensor_from_rosbag
+colcon build --packages-select zed_calibration
+
 source install/setup.bash
 
 echo "======================================================"
